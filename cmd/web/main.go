43fbbb7af2f9c2a6755f6a2270dfbe79eef7e785
bigpaulie <bigpaulie25ro@yahoo.com>
2022-09-18 11:25:35 +0300

    Adding CORS configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bigpaulie/go-todo/pkg/models"
 	"github.com/bigpaulie/go-todo/pkg/routing"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
@@ -23,8 +24,20 @@ func main() {
 	// initialize gin framework
 	router := gin.Default()
 
+	// Setup Middleware
+	setupCorsMiddleWare(router)
+
 	// initialize routes
 	routing.SetupApiRouting(router)
 
 	log.Fatalln(router.Run())
+}
+
+func setupCorsMiddleWare(e *gin.Engine) {
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowCredentials = true
+
+	e.Use(cors.New(config))
 }
